c4b0dc5a41e29a538e695df5be0b6c0752c79658
SteveNdegwa <stevencallistus19@gmail.com>
2025-03-22 14:08:38 +0300

    feat: add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import State, NotificationType, System, NotificationTemplate, Provider, ProviderConfig, Notification
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date_modified', 'date_created')
+    search_fields = ('id', 'name', 'description')
+
+@admin.register(NotificationType)
+class NotificationTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date_modified', 'date_created')
+    search_fields = ('id', 'name', 'description')
+
+@admin.register(System)
+class SystemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'description', 'email_signature', 'sms_signature', 'default_from_email', 'date_modified',
+        'date_created')
+    search_fields = ('id', 'name', 'description', 'email_signature', 'sms_signature', 'default_from_email')
+
+@admin.register(NotificationTemplate)
+class NotificationTemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'code', 'name', 'description', 'type', 'subject', 'body', 'is_active', 'date_modified', 'date_created')
+    list_filter = ('type', 'is_active')
+    search_fields = ('id', 'code', 'name', 'description', 'type__name', 'subject')
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'is_active', 'date_modified', 'date_created')
+    list_filter = ('is_active',)
+    search_fields = ('id', 'name', 'description')
+
+@admin.register(ProviderConfig)
+class ProviderConfigAdmin(admin.ModelAdmin):
+    list_display = ('provider', 'system', 'type', 'config', 'is_active', 'date_modified', 'date_created')
+    list_filter = ('provider', 'system', 'type', 'is_active')
+    search_fields = ('id', 'provider__name', 'system__name', 'type__name')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('system', 'type', 'to', 'template', 'data', 'status', 'date_modified', 'date_created')
+    list_filter = ('system', 'type', 'template', 'status')
+    search_fields = ('id', 'system__name', 'type__name', 'to', 'template__code', 'status__name')
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,96 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(max_length=100, default=uuid.uuid4, unique=True, editable=False, primary_key=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    objects = models.Manager
+
+    class Meta:
+        abstract = True
+
+class GenericBaseModel(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+class State(GenericBaseModel):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-date_created',)
+
+
+class NotificationType(GenericBaseModel):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-date_created',)
+
+class System(GenericBaseModel):
+    email_signature = models.TextField(blank=True)
+    sms_signature = models.CharField(max_length=160, blank=True)
+    default_from_email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-date_created',)
+
+
+class NotificationTemplate(GenericBaseModel):
+    code = models.CharField(max_length=100, unique=True)
+    type = models.ForeignKey(NotificationType, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255, blank=True)
+    body = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-date_created',)
+
+class Provider(GenericBaseModel):
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-date_created',)
+
+class ProviderConfig(BaseModel):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='configs')
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+    type = models.ForeignKey(NotificationType, on_delete=models.CASCADE)
+    config = models.JSONField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "%s %s %s config" % (self.provider.name, self.system.name, self.type.name)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+class Notification(BaseModel):
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+    type = models.ForeignKey(NotificationType, on_delete=models.CASCADE)
+    to = models.CharField(max_length=255)
+    template = models.ForeignKey(NotificationTemplate, on_delete=models.SET_NULL, null=True)
+    data = models.JSONField()
+    status = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s notification to %s" %(self.system.name, self.type.name, self.to)
+
+    class Meta:
+        ordering = ('-date_created',)
+
