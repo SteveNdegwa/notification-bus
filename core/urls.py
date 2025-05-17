df7b9ffb33b421f0c109539ed0b3bff189a3322e
SteveNdegwa <stevencallistus19@gmail.com>
2025-05-17 10:23:13 +0300

    refactor: updates

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import NotifyAPIsManager
 
 urlpatterns = [
     path("send-notification/", NotifyAPIsManager().queue_send_notification, name="send_notification"),
-    path("simple-api-sms-callback/", NotifyAPIsManager().belio_sms_provider_callback, name="simple_api_sms_callback"),
+    path("belio-sms-callback/", NotifyAPIsManager().belio_sms_provider_callback, name="belio_sms_provider_callback"),
 ]
